f35460553c0d88f90f133e0abfe9fa7468d3ed7f
prasoon25bai10821-hue <prasoon.25bai10821@vitbhopal.ac.in>
2026-09-16 16:34:32 +0530

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,30 +1,49 @@
-import java.time.LocalDate;
-import java.io.Serializable;
+import  java.time. LocalDate;
 
+import java.io.Serializable;
 public class Transaction implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     private static int idCounter = 1000;
-    
+
     private String transactionId;
     private String bookId;
+
     private String userId;
     private LocalDate issueDate;
     private LocalDate dueDate;
-
     public Transaction(String bookId, String userId, LocalDate issueDate) {
+
+        // making a new id for every transaction
         this.transactionId = "TXN" + (++idCounter);
+
         this.bookId = bookId;
         this.userId = userId;
         this.issueDate = issueDate;
+
+        // due date is calculated from issue date
         this.dueDate = FineUtils.calculateDueDate(issueDate);
     }
 
-    public String getUserId() { return userId; }
-    public LocalDate getDueDate() { return dueDate; }
+    public String getUserId() {
+       
+        return userId;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
 
     @Override
     public String toString() {
-        return "TXN ID: " + transactionId + " | Book: " + bookId + " | User: " + userId + 
-               " | Issued: " + issueDate + " | Due: " + dueDate;
+
+        // showing all the  important transactions details
+        
+        return "TXN ID: " + transactionId
+                + " | Book: " + bookId
+                + " | User: " + userId
+                + " | Issued: " + issueDate
+                + " | Due: " + dueDate;
     }
 }
