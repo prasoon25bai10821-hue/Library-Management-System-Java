6d26084234ff05ccc6268fa1de7cd93d2b0eab7e
prasoon25bai10821-hue <prasoon.25bai10821@vitbhopal.ac.in>
2026-09-16 15:59:23 +0530

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,34 +1,59 @@
 import java.io.Serializable;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Book implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     private String id;
+
     private String title;
+
     private String author;
-    private boolean isAvailable;
+
+    private boolean available;
+
+    // Stores the user's waiting for this book
     private Queue<String> waitlist;
 
     public Book(String id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
-        this.isAvailable = true;
+        this.available = true;
         this.waitlist = new LinkedList<>();
     }
 
-    public String getId() { return id; }
-    public String getTitle() { return title; }
-    public boolean isAvailable() { return isAvailable; }
-    public void setAvailable(boolean available) { isAvailable = available; }
-    
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // Add a user to  end of the waiting list
     public void addToWaitlist(String userId) {
         waitlist.add(userId);
     }
-    
+
+    // Get the first user waiting for the book
     public String getNextInWaitlist() {
-        return waitlist.poll(); // Returns and removes the first person in line, or null if empty
+        return waitlist.poll();
     }
 
     public int getWaitlistSize() {
@@ -37,7 +62,10 @@ public class Book implements Serializable {
 
     @Override
     public String toString() {
-        return "Book ID: " + id + " | Title: " + title + " | Author: " + author + 
-               " | Available: " + isAvailable + " | Waitlist: " + waitlist.size() + " people";
+        return "Book ID: " + id
+                + " | Title: " + title
+                + " | Author: " + author
+                + " | Available: " + available
+                + " | Waitlist: " + waitlist.size() + " people";
     }
 }
