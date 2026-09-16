b150d572959438dfe86f01a39e1749b8bd3e317b
prasoon25bai10821-hue <prasoon.25bai10821@vitbhopal.ac.in>
2026-09-16 16:40:16 +0530

    Update FineUtils.java

--- src/FineUtils.java
+++ src/FineUtils.java
@@ -1,30 +1,27 @@
-import java.time. LocalDate;
+import java.time.LocalDate;
 
-import  java.time.temporal. ChronoUnit;
-
-
-
+import java.time.temporal.ChronoUnit;
 public class FineUtils {
 
     private static final int FINE_PER_DAY = 50;
 
+    // this method calculate the return date of book
     
-    // The date is been caculating by which the book been returned.
-    
-   
-    public  static LocalDate  calculateDueDate(LocalDate issueDate) {
-        
-        return  issueDate.plusDays( 15);
+    public static LocalDate calculateDueDate(LocalDate issueDate) {
+        return issueDate.plusDays(15);
     }
 
-    // the fine being calculated if book is returned after the due date
-    public static long  calculateFine(LocalDate dueDate, LocalDate returnDate) {
+    // checking how much fine is there if book is returned late
+    public static long calculateFine(LocalDate dueDate, LocalDate returnDate) {
 
-        if (!returnDate.isAfter (dueDate)) {
+        if (!returnDate.isAfter(dueDate)) {
+            
             return 0;
         }
 
-        long  daysLate = ChronoUnit.DAYS.between(dueDate, returnDate);
+        long daysLate = ChronoUnit.DAYS.between(dueDate, returnDate);
+
+        // fine is 50 rupees for each late day
         
         return daysLate * FINE_PER_DAY;
     }
